ef6a8b95755dd0fc59d3cbf3b2c6382dd2d5d7f1
Takahiro Kawaguchi <kawaguchi.takahiro@vareal.co.jp>
2015-09-04 01:02:15 +0900

    自動選出機能追加

--- main.rb
+++ main.rb
@@ -3,10 +3,10 @@ load App.root.join 'db_connection.rb'
 dbm, db = App.dbm, App.db
 loop do
   puts "何をしますか？"
-  print "1 選出 2 履歴 3 個人の履歴 8 メンバー追加 9 メンバー削除 0 終了"
-  select = gets.to_i
+  print "1 選出 2 自動選出 3 全履歴 4 個人の履歴 8 メンバー追加 9 メンバー削除 0 終了"
+  select = gets.to_s.chomp
   case select
-  when 1
+  when "1"
     show_member(db)
     puts "朝礼の対象から外す人のIDをスペース区切りで入力してください"
     print "例 | 1 3 5 : "
@@ -15,34 +15,66 @@ loop do
     sleep(1)
     member = member.sample
     puts member[1]
-  	Notify.say member[1]
+    Notify.say member[1]
     dbm.update_speech(member[0], Time.now.strftime("%Y-%m-%d"))
     puts "タイマーを使用しますか？ : y/n"
     select = gets.chomp
     timer if select == "y"
-  when 2
-		puts "member".center(20, "-")
-		show_member(db)
-		puts "speeches".center(20, "-")
-		show_all_speeches(db)
-  when 3
+  when "2"
+    puts "下記の二人が選出から外れます"
+    show_last_speeches(db)
+    puts "よろしいですか？ : y/n"
+    select = gets.chomp
+    if select == "y" then
+      ids = dbm.last_speeches
+      member = dbm.select_member(ids)
+      sleep(1)
+      member = member.sample
+      puts member[1]
+      Notify.say member[1]
+      dbm.update_speech(member[0], Time.now.strftime("%Y-%m-%d"))
+      puts "タイマーを使用しますか？ : y/n"
+      select = gets.chomp
+      timer if select == "y"
+    end
+  when "3"
+    show_all_speeches(db)
+  when "4"
     print "名前を入力してください : "
     name = gets.to_s.chomp
-		puts "speeches".center(20, "-")
     dbm.history(name)
-  when 8
+  when "8"
     name = gets.to_s.chomp
     dbm.user_entry(name)
-	when 9
-		show_member(db)
-		print "削除したいユーザーのIDを入力してください : "
-		id = gets.chomp
-		dbm.user_delete(id)
-	when 99
-		timer
-	when 999
-  	Notify.say 'time up'
- 	when 0
+  when "9"
+    show_member(db)
+    print "削除したいユーザーのIDを入力してください : "
+    id = gets.chomp
+    dbm.user_delete(id)
+  when "test"
+    show_member(db)
+    puts "選出テスト".center(20, "-")
+    puts "朝礼の対象から外す人のIDをスペース区切りで入力してください"
+    print "例 | 1 3 5 : "
+    ids = gets.to_s.chomp
+    member = dbm.select_member(ids)
+    sleep(1)
+    member = member.sample
+    puts member[1]
+    Notify.say member[1]
+    dbm.update_speech(member[0], Time.now.strftime("%Y-%m-%d"))
+    puts "タイマーを使用しますか？ : y/n"
+    select = gets.chomp
+    timer if select == "y"
+  when "timer"
+    puts "タイマーテスト".center(20, "-")
+    timer
+  when "mic"
+    puts "マイクテスト".center(20, "-")
+    Notify.say 'time up'
+  when "last"
+    show_last_speeches(db)
+  when "0"
     exit
   end
 end
--- show_helper.rb
+++ show_helper.rb
@@ -1,11 +1,29 @@
 def show_member(db)
-  db.execute("SELECT * FROM users") { |id, name|
+  puts "member".center(20, "-")
+  db.execute("SELECT * FROM users") do |id, name|
     puts "#{id} | #{name}"
-  }
+  end
 end
 
 def show_all_speeches(db)
-  db.execute("SELECT * FROM speeches") { |id, user_id, speech_at|
-    puts "#{id} | #{user_id} | #{speech_at}"
-  }
+  puts "speeches".center(20, "-")
+  sql = <<-"SQL"
+      SELECT user_id, speech_at, name
+      FROM speeches LEFT JOIN users ON users.id = speeches.user_id
+      SQL
+  db.execute(sql) do |user_id, speech_at, name|
+    puts "#{user_id} | #{speech_at} | #{name}"
+  end
+end
+
+def show_last_speeches(db)
+  puts "speeches".center(20, "-")
+  sql = <<-"SQL"
+      SELECT user_id, name
+      FROM speeches LEFT JOIN users ON users.id = speeches.user_id
+      ORDER BY speeches.id DESC LIMIT 2
+      SQL
+  db.execute(sql).reverse.each do |user_id, name|
+    puts "#{user_id} | #{name}"
+  end
 end
