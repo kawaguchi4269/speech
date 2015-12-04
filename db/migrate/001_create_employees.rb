14c9feaf09c0699d05eaf06affb4cdae9a0dc919
Ken Sonoda <ken.soda0@gmail.com>
2015-12-05 01:48:37 +0900

    Squash

--- db/migrate/001_create_employees.rb
+++ db/migrate/001_create_employees.rb
@@ -1,11 +1,12 @@
 class CreateEmployees < ActiveRecord::Migration
-  def up
+  def change
     create_table :employees do |t|
       t.string :name
     end
-  end
 
-  def down
-    drop_table :employees
+    create_table :speeches do |t|
+      t.datetime :speech_at
+      t.references :employee
+    end
   end
 end
